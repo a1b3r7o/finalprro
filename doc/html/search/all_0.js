87217f0866706428c2c8898827f6aeb7611d7d23
Alberto Vazquez <a1b3r7ov4z@gmail.com>
2021-11-24 11:08:41 +0100

    documentacion2

---
+++ doc/html/search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['_24method_0',['$method',['../index_8php.html#a12661b2fc0f57f97e30a1620889ce9c6',1,'index.php']]],
+  ['_24router_1',['$router',['../index_8php.html#ad77f00d881c7c3dc8bd1749c7174de92',1,'index.php']]],
+  ['_24url_2',['$url',['../index_8php.html#acf215f34a917d014776ce684a9ee8909',1,'index.php']]]
+];
--- doc/html/search/searchdata.js
+++ doc/html/search/searchdata.js
@@ -1,12 +1,21 @@
 var indexSectionsWithContent =
 {
+  0: "$i",
+  1: "i",
+  2: "$"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "files",
+  2: "variables"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Files",
+  2: "Variables"
 };
 
